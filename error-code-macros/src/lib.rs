db69f9ba48e8566025a64cdd51efa17657b3195b
bjmayor <415074476@qq.com>
2024-05-12 23:14:08 +0800

    feature: add example demonstrate how to use ToErrorInfo macro

---
+++ error-code-macros/src/lib.rs
@@ -0,0 +1,10 @@
+use error_info::process_error_info;
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+mod error_info;
+
+#[proc_macro_derive(ToErrorInfo, attributes(error_info))]
+pub fn derive_to_error_info(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    process_error_info(input).into()
+}
